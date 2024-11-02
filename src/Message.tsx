39f6bbeb513ef7e845bdeec42c907118b08086f3
Maksym Maruda <max26704@gmail.com>
2024-11-03 00:07:32 +0200

    started the project

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+function Massage() {
+    //JSX code: Java Script + XML
+    const name = 'Max'
+
+    if (name) 
+        return <h1>Hello {name}</h1>;
+
+    return <h1>Hello World</h1>
+}
+
+export default Massage;
